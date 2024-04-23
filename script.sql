23bd8424bb88f712579feae722314f80bc04ace2
Aluno <aluno@IPIRANGA.FATEC>
2024-04-23 10:12:44 -0300

    feat(cursor-unbound): cálculo de preço médio por país

--- script.sql
+++ script.sql
@@ -1,29 +1,50 @@
-CREATE TABLE vinho (
-    cd_vinho SERIAL PRIMARY KEY,
-    country VARCHAR(1000),
-    description TEXT,
-    designation VARCHAR(200),
-    points INTEGER,
-    price NUMERIC,
-    province VARCHAR(200),
-    region_1 VARCHAR(200),
-    region_2 VARCHAR(200),
-    taster_name VARCHAR(200),
-    taster_twitter_handle VARCHAR(200),
-    title VARCHAR(200),
-    variety VARCHAR(200),
-    winery VARCHAR(200)
-);
+DO $$
+DECLARE
+    country_nome RECORD;
+    media NUMERIC;
+BEGIN
+    FOR country_nome IN
+        SELECT DISTINCT country FROM vinho
+    LOOP
+        EXECUTE '
+            SELECT AVG(price) FROM vinho WHERE country = $1'
+        INTO media
+        USING country_nome.country;
 
-select * from vinho
+        RAISE NOTICE 'A média do valor do vinho no país % é R$ %', country_nome.country, media;
+    END LOOP;
+END $$;
 
-ALTER TABLE vinho DROP COLUMN designation;
-ALTER TABLE vinho DROP COLUMN province;
-ALTER TABLE vinho DROP COLUMN region_1;
-ALTER TABLE vinho DROP COLUMN region_2;
-ALTER TABLE vinho DROP COLUMN taster_name;
-ALTER TABLE vinho DROP COLUMN taster_twitter_handle;
-ALTER TABLE vinho DROP COLUMN title;
-ALTER TABLE vinho DROP COLUMN variety;
-ALTER TABLE vinho DROP COLUMN winery;
+
+
+
+-- CREATE TABLE vinho (
+--     cd_vinho SERIAL PRIMARY KEY,
+--     country VARCHAR(1000),
+--     description TEXT,
+--     designation VARCHAR(200),
+--     points INTEGER,
+--     price NUMERIC,
+--     province VARCHAR(200),
+--     region_1 VARCHAR(200),
+--     region_2 VARCHAR(200),
+--     taster_name VARCHAR(200),
+--     taster_twitter_handle VARCHAR(200),
+--     title VARCHAR(200),
+--     variety VARCHAR(200),
+--     winery VARCHAR(200)
+-- );
+
+-- select * from vinho
+
+-- ALTER TABLE vinho DROP COLUMN designation;
+-- ALTER TABLE vinho DROP COLUMN province;
+-- ALTER TABLE vinho DROP COLUMN region_1;
+-- ALTER TABLE vinho DROP COLUMN region_2;
+-- ALTER TABLE vinho DROP COLUMN taster_name;
+-- ALTER TABLE vinho DROP COLUMN taster_twitter_handle;
+-- ALTER TABLE vinho DROP COLUMN title;
+-- ALTER TABLE vinho DROP COLUMN variety;
+-- ALTER TABLE vinho DROP COLUMN winery;
+
 
